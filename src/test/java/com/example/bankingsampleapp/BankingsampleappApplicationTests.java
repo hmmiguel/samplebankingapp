4b98d41510f99a8ce34cf7d4d35ec36820bcbce2
Hernandez Mendoza, Miguel <m.hernandez.mendoza@accenture.com>
2023-10-19 22:13:45 +0000

    all

---
+++ src/test/java/com/example/bankingsampleapp/BankingsampleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankingsampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingsampleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
